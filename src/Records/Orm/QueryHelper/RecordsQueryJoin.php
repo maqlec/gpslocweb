48b99c7bc6281d44216b7524779a43499414b9bf
Maciej Pawłowski <mac@macu.eu>
2017-05-17 22:30:50 +0200

    dao render

---
+++ src/Records/Orm/QueryHelper/RecordsQueryJoin.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Records\Orm\QueryHelper;
+
+/**
+ * @method \Records\Orm\RecordsQuery on($localKeyName, $joinedKeyName = 'id')
+ */
+class RecordsQueryJoin extends \Mmi\Orm\QueryHelper\QueryJoin
+{
+
+}
---
+++ src/Records/Orm/RecordsRecord.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Records\Orm;
+
+class RecordsRecord extends \Mmi\Orm\Record
+{
+
+    public $id;
+    public $imei;
+    public $timestamp;
+    public $latitude;
+    public $longitude;
+    public $altitude;
+    public $angle;
+    public $satellites;
+    public $speed;
+
+}
